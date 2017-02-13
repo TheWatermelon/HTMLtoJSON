089c5acc23f961b9d9e80bc5856bef90dc086c86
TheWatermelon <bastien.chapusot@gmx.com>
2017-02-13 17:43:10 +0100

    tests sur HTMLCleaner.py

--- HTMLCleaner.py
+++ HTMLCleaner.py
@@ -3,78 +3,8 @@
 import getopt
 import sys
 
-from HTMLParser import Jeton
 from HTMLParser import Lexical
-
-
-############################################
-# str_to_list(string, delimiter) -> list : #
-############################################
-# Parametres :                             #
-#   - string : la chaine a decouper        #
-#   - delimiter : le delimiteur            #
-############################################
-# Renvoie une liste de tous les elements   #
-# separes par le delimiteur dans la chaine #
-############################################
-def str_to_list(string, delimiter) -> list:
-    liste = []
-    tuple_part = string.partition(delimiter)
-    # cas string avec delimiter
-    while tuple_part[1] != '':
-        liste.append(tuple_part[0])
-        tuple_part = tuple_part[2].partition(delimiter)
-    # cas string sans delimiter
-    if tuple_part[1] == '' and tuple_part[0] != '':
-        liste.append(tuple_part[0])
-    return liste
-
-
-################################################
-# extract_parameters_from_html(line) -> list : #
-################################################
-# Parametres :                                 #
-#  - balise : la balise ouvrante html          #
-################################################
-# Renvoie la liste des parametres plus le type #
-# de la balise                                 #
-################################################
-def extract_parameters_from_html(balise) -> list:
-    param_liste = []
-    type_balise = True
-    type_content = ''
-    key = False
-    key_content = ''
-    value = False
-    value_content = ''
-    for char in balise:
-        if char == ' ' or char == '>':
-            if type_balise:
-                type_balise = False
-                param_liste.append(('type', type_content))
-            if not value:
-                key = True
-            else:
-                value_content += ' '
-            if key_content != '' and value_content != '' and not value:
-                param_liste.append((key_content, value_content))
-                key_content = ''
-                value_content = ''
-            continue
-        if char == '"':
-            value = not value
-            continue
-        if type_balise:
-            if char != '<':
-                type_content += char
-        if value:
-            value_content += char
-        if key:
-            if char == '=':
-                key = False
-            else:
-                key_content += char
-    return param_liste
+import HTMLUtils
 
 
 def clean_html(inputfile, clean_list) -> str:
@@ -83,20 +13,22 @@ def clean_html(inputfile, clean_list) -> str:
     lex = Lexical(inputfile)
     res = lex.suivant()
     while not lex.estEOF(res):
-        param_list = extract_parameters_from_html(res.representation)
+        param_list = HTMLUtils.extract_parameters_from_html(res.representation)
+        print(param_list)
         if lex.estBaliseOuvrante(res):
             stack.append(param_list[0][1])
         type_to_check = ''
         if len(stack) > 0:
-            type_to_check = stack[len(stack)-1]
-        if len(clean_list) > 0 and clean_list.__contains__(type_to_check) == True:
-            if len(stack) > 0 and lex.estBaliseOuvrante(res):
-                for i in range(0, len(stack)-1):
-                    html_output+='\t'
-            html_output+=res.representation
+            type_to_check = stack[len(stack) - 1]
+        if len(clean_list) > 0 and clean_list.__contains__(type_to_check):
+            print("checking "+type_to_check)
+            if lex.estBaliseOuvrante(res) or lex.estBaliseFermante(res):
+                if param_list[0][1] == type_to_check:
+                    html_output += res.representation + "\n"
+            else:
+                html_output += res.representation + "\n"
         if lex.estBaliseFermante(res):
             stack.pop()
-            html_output+='\n'
         res = lex.suivant()
     return html_output
 
@@ -110,7 +42,7 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, "hi:l:o:", ["ifile=", "clist=", "ofile="])
     except getopt.GetoptError:
-        print('args length :'+str(len(sys.argv)))
+        print('args length :' + str(len(sys.argv)))
         print(usage)
         sys.exit(2)
     if len(sys.argv) < 5:
@@ -125,17 +57,16 @@ def main(argv):
         elif opt in ("-o", "--ofile"):
             outputfile = arg
         elif opt in ("-l", "--wlist"):
-            clean_list = str_to_list(arg, ',')
+            clean_list = HTMLUtils.str_to_list(arg, ',')
     if inputfile != '':
         html_output = clean_html(inputfile, clean_list)
     if outputfile != '':
         fd = open(outputfile, 'w')
         fd.write(html_output)
-        print("HTML file cleaned into "+outputfile+" !")
+        print("HTML file cleaned into " + outputfile + " !")
     else:
         print(html_output)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
--- HTMLUtils.py
+++ HTMLUtils.py
@@ -2,8 +2,6 @@
 
 from HTMLParser import Lexical
 
-
-class HTMLUtils:
 ############################################
 # str_to_list(string, delimiter) -> list : #
 ############################################
@@ -14,17 +12,17 @@ class HTMLUtils:
 # Renvoie une liste de tous les elements   #
 # separes par le delimiteur dans la chaine #
 ############################################
-    def str_to_list(self, string, delimiter) -> list:
-        liste = []
-        tuple_part = string.partition(delimiter)
-        # cas string avec delimiter
-        while tuple_part[1] != '':
-            liste.append(tuple_part[0])
-            tuple_part = tuple_part[2].partition(delimiter)
-        # cas string sans delimiter
-        if tuple_part[1] == '' and tuple_part[0] != '':
-            liste.append(tuple_part[0])
-        return liste
+def str_to_list(string, delimiter) -> list:
+    liste = []
+    tuple_part = string.partition(delimiter)
+    # cas string avec delimiter
+    while tuple_part[1] != '':
+        liste.append(tuple_part[0])
+        tuple_part = tuple_part[2].partition(delimiter)
+    # cas string sans delimiter
+    if tuple_part[1] == '' and tuple_part[0] != '':
+        liste.append(tuple_part[0])
+    return liste
 
 
 ###########################################
@@ -36,24 +34,24 @@ class HTMLUtils:
 # Renvoie une liste des balises contenues #
 # dans le fichier source                  #
 ###########################################
-    def get_html_balises(self, inputfile) -> list:
-        liste = []
-        stack = []
-        lex = Lexical(inputfile)
+def get_html_balises(inputfile) -> list:
+    liste = []
+    stack = []
+    lex = Lexical(inputfile)
+    res = lex.suivant()
+    while not lex.estEOF(res):
+        if lex.estBaliseOuvrante(res) or lex.estContenu(res):
+            stack.append(res)
+        if lex.estBaliseFermante(res):
+            jeton1 = stack.pop()
+            if lex.estContenu(jeton1):
+                jeton2 = stack.pop()
+                balise = jeton2.representation + jeton1.representation + res.representation
+            else:
+                balise = jeton1.representation + res.representation
+            liste.append(balise)
         res = lex.suivant()
-        while not lex.estEOF(res):
-            if lex.estBaliseOuvrante(res) or lex.estContenu(res):
-                stack.append(res)
-            if lex.estBaliseFermante(res):
-                jeton1 = stack.pop()
-                if lex.estContenu(jeton1):
-                    jeton2 = stack.pop()
-                    balise = jeton2.representation + jeton1.representation + res.representation
-                else:
-                    balise = jeton1.representation + res.representation
-                liste.append(balise)
-            res = lex.suivant()
-        return liste
+    return liste
 
 
 #################################################
@@ -67,22 +65,22 @@ class HTMLUtils:
 # Renvoie le contenu qui se trouve entre les    #
 # deux balises                                  #
 #################################################
-    def extract_content_from_html_line(self, line) -> str:
-        balise = False
-        in_content = False
-        content = ''
-        for char in line:
-            if char == '<':
-                balise = True
-                in_content = False
-            elif char == '>':
-                balise = False
-            else:
-                if not balise:
-                    in_content = True
-            if in_content:
-                content += char
-        return content
+def extract_content_from_html_line(line) -> str:
+    balise = False
+    in_content = False
+    content = ''
+    for char in line:
+        if char == '<':
+            balise = True
+            in_content = False
+        elif char == '>':
+            balise = False
+        else:
+            if not balise:
+                in_content = True
+        if in_content:
+            content += char
+    return content
 
 
 ################################################
@@ -94,42 +92,42 @@ class HTMLUtils:
 # Renvoie la liste des parametres plus le type #
 # de la balise                                 #
 ################################################
-    def extract_parameters_from_html(self, balise) -> list:
-        param_liste = []
-        type_balise = True
-        type_content = ''
-        key = False
-        key_content = ''
-        value = False
-        value_content = ''
-        for char in balise:
-            if char == ' ' or char == '>':
-                if type_balise:
-                    type_balise = False
-                    param_liste.append(('type', type_content))
-                if not value:
-                    key = True
-                else:
-                    value_content += ' '
-                if key_content != '' and value_content != '' and not value:
-                    param_liste.append((key_content, value_content))
-                    key_content = ''
-                    value_content = ''
-                continue
-            if char == '"':
-                value = not value
-                continue
+def extract_parameters_from_html(balise) -> list:
+    param_liste = []
+    type_balise = True
+    type_content = ''
+    key = False
+    key_content = ''
+    value = False
+    value_content = ''
+    for char in balise:
+        if char == ' ' or char == '>':
             if type_balise:
-                if char != '<':
-                    type_content += char
-            if value:
-                value_content += char
-            if key:
-                if char == '=':
-                    key = False
-                else:
-                    key_content += char
-        return param_liste
+                type_balise = False
+                param_liste.append(('type', type_content))
+            if not value:
+                key = True
+            else:
+                value_content += ' '
+            if key_content != '' and value_content != '' and not value:
+                param_liste.append((key_content, value_content))
+                key_content = ''
+                value_content = ''
+            continue
+        if char == '"':
+            value = not value
+            continue
+        if type_balise:
+            if char != '<' and char != '/':
+                type_content += char
+        if value:
+            value_content += char
+        if key:
+            if char == '=':
+                key = False
+            else:
+                key_content += char
+    return param_liste
 
 
 #############################################
@@ -141,22 +139,22 @@ class HTMLUtils:
 #############################################
 # Renvoie un element JSON a partir de line  #
 #############################################
-    def html_line_to_json_element(self, line, white_list) -> dict:
-        element = {}
-        if line[-1:] == '\n':
-            line = line[:-1]
-        if line != '':
-            params = self.extract_parameters_from_html(line)
-            params.append(('data', self.extract_content_from_html_line(line)))
-            params.append(('dataHTML', line))
-            for value in params:
-                # checks whether the type is in the white list
-                if value[0] == 'type':
-                    if len(white_list) > 0 and white_list.__contains__(value[1]) == False:
-                       return {}
-                # key => value[0], value => value[1]
-                element[value[0]] = value[1]
-            if element.__contains__('data-area'):
-                return element
-        return {}
+def html_line_to_json_element(line, white_list) -> dict:
+    element = {}
+    if line[-1:] == '\n':
+        line = line[:-1]
+    if line != '':
+        params = extract_parameters_from_html(line)
+        params.append(('data', extract_content_from_html_line(line)))
+        params.append(('dataHTML', line))
+        for value in params:
+            # checks whether the type is in the white list
+            if value[0] == 'type':
+                if len(white_list) > 0 and white_list.__contains__(value[1]) == False:
+                   return {}
+            # key => value[0], value => value[1]
+            element[value[0]] = value[1]
+        if element.__contains__('data-area'):
+            return element
+    return {}
 
